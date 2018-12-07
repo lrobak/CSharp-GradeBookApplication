46e8ce0d26b89bb89712bc696ea7b9fdcddefc1b
lrobak <45694002+lrobak@users.noreply.github.com>
2018-12-07 18:09:21 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,42 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char result;
+
+            if(5 < Students.Count)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            List<double> grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grades[threshold-1] <= averageGrade)
+            {
+                result = 'A';
+            }
+            else if(grades[(2*threshold)-1] <= averageGrade)
+            {
+                result = 'B';
+            }
+            else if (grades[(3 * threshold) - 1] <= averageGrade)
+            {
+                result = 'C';
+            }
+            else if (grades[(4 * threshold) - 1] <= averageGrade)
+            {
+                result = 'D';
+            }
+            else
+            {
+                result = 'F';
+            }
+
+            return result;
         }
     }
 }
